1fdd7ec7a76c15afdb1bc91ed141c816d4653399
EricksonDutra <ericksond10@gmail.com>
2021-06-22 23:15:07 -0400

    aula 03 finalizada, layout construído.]

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -36,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
                   height: 300,
                 )),
             Positioned(
-              bottom: size.height * 0.25,
+              bottom: size.height * 0.20,
               left: 0,
               right: 0,
               child: Column(
@@ -58,7 +60,7 @@ class _LoginPageState extends State<LoginPage> {
                           const EdgeInsets.only(left: 40, right: 40, top: 40),
                       child: SocialLoginButton(
                         onTap: () {
-                          print("Click!!");
+                          controller.googleSignIn(context);
                         },
                       ),
                     ),
