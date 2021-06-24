61daf4c953967df430d4dc872ca75e1d21e0713e
EricksonDutra <ericksond10@gmail.com>
2021-06-24 00:49:17 -0400

    aula 04 - mlkit_google

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
@@ -6,17 +8,25 @@ import 'package:payflow/modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
